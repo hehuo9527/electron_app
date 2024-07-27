f6047db4ed727f9387d6b65d726c7803423b69cd
hehuo9527 <943280632@qq.com>
2024-07-27 22:36:10 +0800

    添加obs-websocket js获取到obs推流数据

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Versions from './components/Versions.vue'
+import OBSWebSocket, { EventSubscription, OBSEventTypes, OBSRequestTypes } from 'obs-websocket-js'
+const obs = new OBSWebSocket()
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
 function WsTest() {
@@ -8,6 +10,35 @@ function WsTest() {
   ws.onopen = function (evt) {
     console.log('Connection open ...')
     ws.send('Love from vue!')
+  }
+}
+
+async function ObsTest() {
+  try {
+    const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
+      'ws://192.168.1.23:4455',
+      'Hcs8Pczy4JVDfnR2',
+      {
+        rpcVersion: 1
+      }
+    )
+    console.log(`Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`)
+    // 获取推流图像数据
+    // 发送截图命令
+    obs.call('GetVersion').then((data) => {
+      console.log('getversion', data)
+    })
+    obs
+      .call('GetSourceScreenshot', {
+        sourceName: '显示器采集',
+        imageFormat: 'png'
+      })
+      .then((data) => {
+        console.log(data.imageData)
+      })
+  } catch (error) {
+    console.log('obs err')
+    console.error('Failed to connect', error)
   }
 }
 </script>
@@ -31,6 +62,9 @@ function WsTest() {
     </div>
     <div class="action">
       <a target="_blank" rel="noreferrer" @click="WsTest">WsTest</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ObsTest">ObsTest</a>
     </div>
   </div>
   <Versions />
