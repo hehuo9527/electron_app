0cb58929535088d7388429c71f5acedbabfa92eb
qiulin.yang <qiulin.yang@sony.com>
2024-08-02 17:38:22 +0800

    调用exe

--- src/main/sdk-handle.ts
+++ src/main/sdk-handle.ts
@@ -1,9 +1,10 @@
-import { execFileSync } from 'child_process'
-import { tr } from 'element-plus/es/locale'
+import { exec, execSync } from 'child_process'
 
 export function startSDK() {
   try {
-    const pythonProcess = execFileSync('python', ['test_socket.py'])
+    // TODO 这里Path应该读环境变量
+    const sdkProcess = exec('C:\\Users\\7000031561\\Desktop\\CameraSDK_Release\\CameraSDK.exe')
+    console.log(sdkProcess)
   } catch (error) {
     console.error('Error executing file:', error)
   }
