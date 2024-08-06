ddebc7242d55441369ef8658f97d454278382f63
Qiulin.Yang <Qiulin.Yang@sony.com>
2024-08-06 13:15:16 +0800

    format code

--- src/main/sdk-handle.ts
+++ src/main/sdk-handle.ts
@@ -1,12 +1,12 @@
-import { ChildProcess, exec } from 'child_process';
-import * as path from 'path';
-import * as fs from'fs'
+import { ChildProcess, exec } from 'child_process'
+import * as path from 'path'
+import * as fs from 'fs'
 export function startSDK(): ChildProcess | null {
-    let cppProcess: ChildProcess | null = null;
-    const rootPath = path.resolve(process.cwd(), '../../');
+  let cppProcess: ChildProcess | null = null
+  const rootPath = path.resolve(process.cwd(), '../../')
 
-    try {
-        console.log("rootPath",rootPath)
+  try {
+    console.log('rootPath', rootPath)
     //     fs.writeFile('1111111111.txt',  path.join(rootPath,  '\\CameraSDK_Release\\CameraSDK.exe'), 'utf8', (err) => {
     //       if (err) {
     //           console.error(err);
@@ -14,11 +14,11 @@ export function startSDK(): ChildProcess | null {
     //           console.log('文件写入成功！');
     //       }
     //   });
-        const sdkPath = path.join(rootPath,  '\\CameraSDK_Release\\CameraSDK.exe');
-        cppProcess = exec(sdkPath);
-        return cppProcess;
-    } catch (error) {
-        console.error('exec Failed', error);
-        return null;
-    }
+    const sdkPath = path.join(rootPath, '\\CameraSDK_Release\\CameraSDK.exe')
+    cppProcess = exec(sdkPath)
+    return cppProcess
+  } catch (error) {
+    console.error('exec Failed', error)
+    return null
+  }
 }
