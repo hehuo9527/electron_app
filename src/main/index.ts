c13a28ef73a6c06b33d374300d2163cfa2255593
hehuo9527 <943280632@qq.com>
2024-08-13 23:34:48 +0800

    sync get socket data

--- src/main/index.ts
+++ src/main/index.ts
@@ -64,7 +64,7 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.sony.csc')
+  electronApp.setAppUserModelId('com.sss.csc')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
